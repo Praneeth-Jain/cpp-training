f3fc0cfe67935a15645d0f0ea424f6e41603dc55
Praneeth Jain <praneethjain15@gmail.com>
2024-09-06 00:38:10 +0530

    Day 2-afn

---
+++ assignment/Student.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Student{
+    public :
+    string firstName,lastName,usn,branch,gender;
+    int age;
+
+    Student(){
+        cout<<"Student Object is created \n\n";
+    }
+
+    void getData(){
+        cout<<"\n\nEnter the first and last name : ";
+        cin>>firstName>>lastName;
+        cout<<"\n\nEnter the USN  : ";
+        cin>>usn;
+        cout<<"\n\nEnter Your Branch : ";
+        cin>>branch;
+        cout<<"\n\nEnter the gender : ";
+        cin>>gender;
+        cout<<"\n\nEnter the Age : ";
+        cin>>age;
+    }
+    void displayData(){
+        cout<<"\n\n_______  Student Data _______________"<<endl;
+        cout<<firstName<<" "<<lastName<<"\t"<<usn<<"\t"<<branch<<"\t"<<gender<<"\t"<<age;
+    }
+};
+
+int main(){
+    Student st;
+    st.getData();
+    st.displayData();
+    return 0;
+}
